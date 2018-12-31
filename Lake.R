847156e4d77564c5244687ac4849339b8e9356e0
Leanne <leanne.thng@gmail.com>
2018-12-30 21:30:37 -0800

    Update Lake.R

--- Lake.R
+++ Lake.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(lubridate)
+library(ggplot2)
 
 
 
@@ -17,7 +18,42 @@ str(mendota)
 
 mendota$closed_date <- as.Date(paste(mendota$CLOSED, mendota$START.YEAR), format = '%d %b %Y')
 
-mendota$opening_date <- as.Date(paste(mendota$OPENED, mendota$START.YEAR), format = '%d %b %Y')
+mendota$opening_date <- as.Date(paste(mendota$OPENED, mendota$END.YEAR), format = '%d %b %Y')
+
+mendota$date_diff <- as.Date(as.character(mendota$opening_date), format="%Y-%m-%d")- as.Date(as.character(mendota$closed_date), format="%Y-%m-%d")
+
+
+
+# if closed date is January to April then add 1 onto the year
+
+x <- as.Date('0014-06-30')
+x
+year(x)
+year(x) <- year(x)+1
+x
+
+
+month(mendota$closed_date) < 4 
+
+
+closed <- mendota$closed_date
+closed_month <- month(mendota$closed_date)
+closed_year <- year(mendota$closed_date)
+closed_month
+
+
+for (i in closed){
+    
+    ifelse(closed_month < 4, 1,0)
+     
+   
+}
+warnings()
+
+
+
+
+str(mendota)
 
 barplot(height = mendota$DAYS,
         names.arg = mendota$START.YEAR,
@@ -28,5 +64,24 @@ barplot(height = mendota$DAYS,
         ylim = c(0, 160)
 )
 
+ggplot(mendota, aes(START.YEAR, DAYS)) + 
+    geom_point() +
+    geom_smooth(model = loess)
+
+
+ggplot(mendota, aes(month(closed_date))) + 
+    geom_bar()
+
+
+# TESTING --------
+###
+
+# survey <- data.frame(date=c("2012/07/26","2012/07/25"),tx_start=c("2012/01/01","2012/01/01"))
+
+# survey$date_diff <- as.Date(as.character(survey$date), format="%Y/%m/%d")-
+#    as.Date(as.character(survey$tx_start), format="%Y/%m/%d")
+# survey
+
+###
 
 
